a70fb527e4b38495a3952e57c9965c2c5581d1d7
Joshwawo <masacota_masacota@hotmail.com>
2022-08-07 16:15:20 -0700

    Add:Variables de entorno

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ import express from "express";
 import cors from "cors";
 import db from "./database/db.js";
 import blogRoutes from "./routes/routes.js";
+// import dotenv from "dotenv";
+
 const app = express();
+// dotenv.config();
 
 const PORT = process.env.PORT || 8000;
+
+
 
 app.use(cors());
 app.use(express.json());
--- database/db.js
+++ database/db.js
@@ -1,14 +1,17 @@
 import {Sequelize} from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 // 'blog','root', 'root',
 const db = new Sequelize({
     host: process.env.HOST,
-    username: process.env.USERNAME,
-    database: process.env.DATABASE,
+    username: process.env.USER,
+    database: process.env.BDNAME,
     password: process.env.PASSWORD,
     dialect: 'mysql',
     logging: false,
 });
 
+// console.log(db.config.database);
 
 
 
