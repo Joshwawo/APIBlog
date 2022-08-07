ffecdd39da92ac04198ddde583a96db9262055f7
Joshwawo <masacota_masacota@hotmail.com>
2022-08-07 15:19:46 -0700

    fix

--- database/db.js
+++ database/db.js
@@ -1,10 +1,24 @@
 import {Sequelize} from "sequelize";
 
+// const db = new Sequelize({
+//     username: 'root',
+//     host: 'localhost',
+//     database: 'blog',
+//     password: 'root',
+//     dialect: 'mysql',
+//     logging: false,
+// });
 
-const db = new Sequelize('blog','root', 'root',{
-    host: 'localhost',
+// 'blog','root', 'root',
+const db = new Sequelize({
+    host: 'us-cdbr-east-06.cleardb.net',
+    username: 'b2d3fcdff1bcef',
+    database: 'heroku_dc4e932dba01a66',
+    password: '444ca69e',
     dialect: 'mysql',
     logging: false,
 });
+
+
 
 export default db;
