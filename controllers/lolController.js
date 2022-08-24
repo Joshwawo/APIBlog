b59295f50ab0681e2aad330051a01dbb52ff8d37
Joshwawo <masacota_masacota@hotmail.com>
2022-08-23 23:59:14 -0700

    add:middleware cors acao

--- controllers/lolController.js
+++ controllers/lolController.js
@@ -23,14 +23,19 @@ const fetchPlayer = async (playerName) => {
     .catch((error) => console.log(error));
 };
 
-export const lolChamps = (req, res) => {
-  res.json({ message: "hola desde lolChamps" });
+export const lolChamps = (req, res, next) => {
+
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173/");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+    res.json({ message: "hola desde lolChamps" });
 };
 
 export const lolAllChamps = async (req, res, next) => {
   // res.json({message:'Hola desde allChamps'})
   //asd
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173/");
+//   res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173/");
   res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET,POST, PUT, DELETE, OPTIONS")
