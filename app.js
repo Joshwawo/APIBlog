28d41ed28d50e57df52f99cdcdb869ca51110ff3
Joshwawo <masacota_masacota@hotmail.com>
2022-08-24 01:39:29 -0700

    add:middleware cors acao

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import db from "./database/db.js";
 import blogRoutes from "./routes/routes.js";
 import cardRoutes from "./routes/routes2.js";
 import lolRoutes from "./routes/routes3.js";
-import methodOverride from "method-override";
 
 // import dotenv from "dotenv";
 
@@ -15,39 +14,39 @@ const PORT = process.env.PORT || 8000;
 app.use(cors({
     origin: ["https://app-full-stack.vercel.app/","http://127.0.0.1:5173/"]
 }));
-app.use(methodOverride());
+// app.use(methodOverride());
 app.use(express.json());
 
 //Proteger la ruta con cors
-export const whitelist = [
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "177.228.67.117",
-  "192.168.1.4",
-  "http://localhost:3307/",
-  "http://localhost:3307/lol",
-  "http://localhost:3307/testing",
-  "http://127.0.0.1:5173/",
-  "http://localhost:3307/lol",
-  "http://127.0.0.1:5173/lol",
-  "http://localhost:3307/testing",
-  "2806:263:c487:539:e92b:72e2:645d:53b7",
-  "https://app-full-stack.vercel.app/"
-];
+// export const whitelist = [
+//   "http://localhost:3000",
+//   "http://localhost:5173",
+//   "177.228.67.117",
+//   "192.168.1.4",
+//   "http://localhost:3307/",
+//   "http://localhost:3307/lol",
+//   "http://localhost:3307/testing",
+//   "http://127.0.0.1:5173/",
+//   "http://localhost:3307/lol",
+//   "http://127.0.0.1:5173/lol",
+//   "http://localhost:3307/testing",
+//   "2806:263:c487:539:e92b:72e2:645d:53b7",
+//   "https://app-full-stack.vercel.app/"
+// ];
 
-export const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(
-        new Error(
-          "Bloqueado por CORS, si quieres acceder a este recurso, ponte en contacto con el administrador"
-        )
-        );
-      }
-  },
-};
+// export const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(
+//         new Error(
+//           "Bloqueado por CORS, si quieres acceder a este recurso, ponte en contacto con el administrador"
+//         )
+//         );
+//       }
+//   },
+// };
 
 app.use("/blogs", blogRoutes);
 app.use("/clash", cardRoutes);
